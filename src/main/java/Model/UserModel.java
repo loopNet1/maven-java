59fcc3918a9b8aa3d7c2e2d37b51c61bdf4b0437
omkar <omkar.phadtare@fineshift.com>
2023-06-07 09:37:36 +0530

    model added

---
+++ src/main/java/Model/UserModel.java
@@ -0,0 +1,26 @@
+package Model;
+
+public class UserModel {
+    
+    int num;
+    String name;
+    String add;
+    public int getNum() {
+        return num;
+    }
+    public void setNum(int num) {
+        this.num = num;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAdd() {
+        return add;
+    }
+    public void setAdd(String add) {
+        this.add = add;
+    }
+}
